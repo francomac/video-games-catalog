25c91af498bf36149a00cf82182be8777e9ff911
francomac <franco.mcarballo@outlook.com>
2024-03-25 12:57:29 -0600

    new expensesFilter Component

--- src/expense-tracker/components/ExpensesTracker/ExpenseFilter.tsx
+++ src/expense-tracker/components/ExpensesTracker/ExpenseFilter.tsx
@@ -1,5 +1,19 @@
-const ExpenseFilter = () => {
-	return <div>ExpenseFilter</div>;
+interface Props {
+	onSelectCategories: (category: string) => void;
+}
+
+const ExpenseFilter = ({ onSelectCategories }: Props) => {
+	return (
+		<select
+			className='form-select'
+			onChange={(e) => onSelectCategories(e.target.value)}
+		>
+			<option value=''>All Groceries</option>
+			<option value='groceries'>Groceries</option>
+			<option value='utilities'>Utilities</option>
+			<option value='entertainment'>Entertainment</option>
+		</select>
+	);
 };
 
 export default ExpenseFilter;
